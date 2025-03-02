c2c645a9dde52f39215421327b6375aaa63fe333
Bfw72886 <otto72886@m365.bfwbb-lernen.de>
2025-03-02 16:15:42 +0100

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reversedArray = [];
+    let reversedString = "";
+    for (const char of stringArray) {
+        reversedArray.unshift(char);
+    }
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
