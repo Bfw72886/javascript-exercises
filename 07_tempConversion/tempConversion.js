f9777fb0d4dbd1a7e39686ad9653633923e3e685
Brian <otto72886@m365.bfwbb-lernen.de>
2025-03-15 18:33:02 +0100

    Complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+// °C = (°F - 32) ÷ (9/5)
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) / (9 / 5);
+
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// °F = °C * 9/5 + 32
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = celsius * (9 / 5) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
