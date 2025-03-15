84fea3d9e97e1bdb4568bbe2ef17275213ae32e1
Brian <otto72886@m365.bfwbb-lernen.de>
2025-03-15 18:11:29 +0100

    Complete exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear = false;
 
+    // Leap years are years divisible by four 
+    if (year % 4 === 0) {
+        isLeapYear = true;
+
+        // However, years divisible by 100 are not leap years
+        // unless they are divisible by 400
+        if (year % 100 === 0 && year % 400 != 0) {
+            isLeapYear = false;
+        }
+    }
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
