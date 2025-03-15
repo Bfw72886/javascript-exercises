95f21168b93dd8a57640204e72498ee370b3ecd0
Brian <otto72886@m365.bfwbb-lernen.de>
2025-03-15 17:53:58 +0100

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let from;
+    let to;
 
+    if (num1 <= num2) {
+        from = num1;
+        to = num2;
+    } else {
+        from = num2;
+        to = num1;
+    }
+
+    if (from < 0) {
+        return "ERROR";
+    }
+
+    if (typeof from != "number" || typeof to != "number") {
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(from) || !Number.isInteger(to)) {
+        return "ERROR";
+    }
+
+    for (let i = from; i <= to; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
