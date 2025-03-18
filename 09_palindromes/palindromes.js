257fe48f1c6e11e7da2e973b0c4eec26ff6a8e91
Brian <otto72886@m365.bfwbb-lernen.de>
2025-03-18 18:23:45 +0100

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let letterArray = word.toLowerCase().replace(/[\s\.\?!,;:'"-]/g, '').split('');
+    let reversedArray = word.toLowerCase().replace(/[\s\.\?!,;:'"-]/g, '').split('').reverse();
+    return reversedArray.join('') === letterArray.join('');
 };
 
 // Do not edit below this line
