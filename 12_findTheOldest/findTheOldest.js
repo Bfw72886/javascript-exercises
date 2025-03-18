83902d8ece41f90c206824a9cfea8e9066a5777d
Brian <otto72886@m365.bfwbb-lernen.de>
2025-03-18 19:43:42 +0100

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(personArray) {
 
+    let currentOldest = personArray[0];
+    let currentYear = new Date().getFullYear();
+
+    for (let person of personArray) {
+        if (!('yearOfDeath' in person)) {
+            person.yearOfDeath = currentYear;
+        } 
+        if (person.yearOfDeath - person.yearOfBirth > currentOldest.yearOfDeath - currentOldest.yearOfBirth) {
+            currentOldest = person;
+        }
+    }
+
+    return currentOldest;
 };
 
 // Do not edit below this line
