24ea2e4f31fdec2d2da2195aaee2f0fa13bdd4e1
Brian <otto72886@m365.bfwbb-lernen.de>
2025-03-18 18:47:29 +0100

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(place) {
+    let fiboArray = [1,1];
 
+    if (+place < 0) {
+        return "OOPS";
+    }
+
+    if (+place === 0) {
+        return 0;
+    }
+
+    for (let i = 2; i < +place; i++) {
+        fiboArray.push(fiboArray[(i - 2)] + fiboArray[(i - 1)]);
+    }
+
+    return fiboArray[fiboArray.length - 1];
 };
 
 // Do not edit below this line
